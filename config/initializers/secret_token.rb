51b4c9615a04d29dfcf19bc2b8db54a208a07b8c
Micah Roberson <Micah.roberson@gmail.com>
2012-04-18 22:11:49 -0400

    first rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Huddleston::Application.config.secret_token = 'be8dbb3675c13b4ba11e76bbed989512cb19f385035adff86c4e3150518619814d3841b683f7c59f7b6ea8e4cd9ada14c4725d10ad3119139d23df75a01e68dc'
